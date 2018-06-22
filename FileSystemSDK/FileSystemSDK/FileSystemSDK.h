67667f1e0da6142d37ff207a399974f42c37c32a
Towhid <m.towhid.islam@gmail.com>
2018-06-22 13:40:15 +0600

    CryptoFile Added

--- FileSystemSDK/FileSystemSDK/FileSystemSDK.h
+++ FileSystemSDK/FileSystemSDK/FileSystemSDK.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FileSystemSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FileSystemSDK/PublicHeader.h>
 
-
+#import <FileSystemSDK/CryptoFile.h>
