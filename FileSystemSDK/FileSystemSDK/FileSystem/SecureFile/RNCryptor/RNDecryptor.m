50d38d05f1e0782f8236bf3fedd14cfd7afe55f6
Towhid Islam <m.towhid.islam@gmail.com>
2021-12-09 16:06:58 +0600

    Updating according to Xcode suggestion.

--- FileSystemSDK/FileSystemSDK/FileSystem/SecureFile/RNCryptor/RNDecryptor.m
+++ FileSystemSDK/FileSystemSDK/FileSystem/SecureFile/RNCryptor/RNDecryptor.m
@@ -164,7 +164,7 @@ static const NSUInteger kPreambleSize = 2;
 {
   dispatch_async(self.queue, ^{
     if (self.hasHMAC) {
-      CCHmacUpdate(&_HMACContext, data.bytes, data.length);
+        CCHmacUpdate(&self->_HMACContext, data.bytes, data.length);
     }
 
     NSError *error = nil;
@@ -311,7 +311,7 @@ static const NSUInteger kPreambleSize = 2;
 
     if (self.hasHMAC) {
       NSMutableData *HMACData = [NSMutableData dataWithLength:self.HMACLength];
-      CCHmacFinal(&_HMACContext, [HMACData mutableBytes]);
+        CCHmacFinal(&self->_HMACContext, [HMACData mutableBytes]);
 
       if (![HMACData rnc_isEqualInConsistentTime:self.inData]) {
         [self cleanupAndNotifyWithError:[NSError errorWithDomain:kRNCryptorErrorDomain
